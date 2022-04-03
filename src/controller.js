62b6da083d875a1daf9dd000139d98dca2749759
zhou quan wang <qwzhou89@icloud.com>
2022-04-03 17:08:29 +0800

    修正url过滤逻辑

--- src/controller.js
+++ src/controller.js
@@ -6,7 +6,7 @@ const renderPrometheusMetric = require('./renderPrometheusMetric');
 
 module.exports = (config) => {
   const app = express();
-  
+
   app.get('/', (req, res) => {
     res.send(`
     <a href="/metrics">/metrics</a> (internal)<br/>
@@ -15,9 +15,9 @@ module.exports = (config) => {
   });
 
   app.get('/metrics', (req, res) => {
-    res.set({'Content-Type': 'text/plain'});
+    res.set({ 'Content-Type': 'text/plain' });
     const measures = internalMetricCounter.getLabels().map(labels => {
-      return Object.assign({value: internalMetricCounter.get(labels)}, labels);
+      return Object.assign({ value: internalMetricCounter.get(labels) }, labels);
     });
     res.send(renderPrometheusMetric({
       name: 'http_json_query_exporter_query_count',
@@ -29,25 +29,25 @@ module.exports = (config) => {
   app.get('/:taskName/metrics', (req, res) => {
     loadConfig(config.configPath).then(config => {
       Promise.all(config.tasks
-        .filter(task =>  req.params.taskName === 'all' || req.params.taskName === task.name)
-        .map(task => req.params.url !== '' && (task.query.url = req.params.url))
+        .filter(task => req.params.taskName === 'all' || req.params.taskName === task.name)
+        .map(task => { !!req.query.url && (task.query.url = req.query.url); return task; })
         .map(runTask)).then(lines => {
-          internalMetricCounter.increment({metric: 'all', state: 'success'});
-          res.set({'Content-Type': 'text/plain'});
+          internalMetricCounter.increment({ metric: 'all', state: 'success' });
+          res.set({ 'Content-Type': 'text/plain' });
           res.send(lines.join('\n'));
         })
         .catch(e => {
           console.error(e);
-          internalMetricCounter.increment({metric: 'all', state: 'error'});
+          internalMetricCounter.increment({ metric: 'all', state: 'error' });
           res.status(500).send(e.stack);
         });
     })
       .catch(e => {
         console.error(e);
-        internalMetricCounter.increment({metric: 'all', state: 'error'});
+        internalMetricCounter.increment({ metric: 'all', state: 'error' });
         res.status(500).send(e.stack);
       });
   });
-  
+
   return app;
 };
